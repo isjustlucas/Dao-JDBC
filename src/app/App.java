06381fe5e45cfc7719ca0ae58723dd3d119abaf9
isjustlucas <vastokinha100@gmail.com>
2026-05-02 00:31:22 -0300

    Insert Implementation.

--- src/app/App.java
+++ src/app/App.java
@@ -29,6 +29,10 @@ public class App {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+        System.out.println("\n === TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(0), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
 
     }
 }
